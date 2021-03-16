e719b7727106c7a8cc2083217358c647187bcbfc
tod619 <trevorodwyer619@hotmail.com>
2021-03-16 16:06:42 +0000

    create updateBigCup function in style.css

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const percentage = document.getElementById("percentage")
 const remained = document.getElementById("remained")
 
 
+updateBigCup()
+
 smallCups.forEach((cup,idx)=> {
     cup.addEventListener("click", ()=> highlightCups(idx))
 })
@@ -23,4 +25,30 @@ function highlightCups(idx) {
             cup.classList.remove("full")
         }
     })
+
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll(".cup-small.full").length
+
+    const totalCups = smallCups.length
+    
+
+    if(fullCups === 0){
+        percentage.style.visibility = "hidden"
+        percentage.style.height = 0
+    }else {
+        percentage.style.visibility = "visible"
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    if(fullCups === totalCups) {
+        remained.style.visibility = "hidden"
+        remained.style.height = 0
+    }else {
+        remained.style.visibility = "visible"
+        litres.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
 }
